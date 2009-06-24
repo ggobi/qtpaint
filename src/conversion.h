5304bdb9647326fc015a9f5cd6bab38c72a197f6
Michael Lawrence <lawrence.michael@gene.com>
2009-06-24 14:41:30 +0000

    Clean up of conversion.cpp/h.

--- src/conversion.h
+++ src/conversion.h
@@ -6,15 +6,15 @@
 
 #include <Rinternals.h>
 
-extern "C" {
-  SEXP asRRect(QRectF rect);
-  SEXP asRMatrix(QMatrix matrix, bool inverted);
-  SEXP asRPoint(QPointF point);
-  
-  QColor *asQColors(SEXP c);
-  QColor asQColor(SEXP c);
-  QMatrix asQMatrix(SEXP m);
-}
+SEXP asRRect(QRectF rect);
+SEXP asRMatrix(QMatrix matrix, bool inverted);
+SEXP asRPoint(QPointF point);
+
+QColor *asQColors(SEXP c);
+QColor asQColor(SEXP c);
+QMatrix asQMatrix(SEXP m);
+
+QMatrix flipYMatrix(qreal max);
 
 // at some point might want to do something special
 #define wrapQGraphicsScene wrapQObject
