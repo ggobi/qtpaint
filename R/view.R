d1dd4f2e1f8b91b421bdd3d0fb097fd2bb888046
Michael Lawrence <lawrence.michael@gene.com>
2010-03-09 14:14:52 +0000

    Fix wrapper for PlotView constructor

--- R/view.R
+++ R/view.R
@@ -1,7 +1,8 @@
 ## convenient R-level constructor, following Layer's precedent
-qplotView <- function(scene, rescale = c("geometry", "transform", "none"),
+qplotView <- function(scene, parent = NULL,
+                      rescale = c("geometry", "transform", "none"),
                       opengl = TRUE)
 {
   rescale <- c(none = 0L, geometry = 1L, transform = 2L)[match.arg(rescale)]
-  Qanviz$PlotView(scene, rescale, opengl)
+  Qanviz$PlotView(scene, parent, rescale, opengl)
 }
